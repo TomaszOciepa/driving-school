e7248e09fd572dda33e8711fed269375fe821c4c
tomaszociepa <tom.ociepa@gmail.com>
2019-02-01 14:44:57 +0100

    addition edit Manager

---
+++ src/main/java/servlet/manager/AccountManagerServlet.java
@@ -0,0 +1,55 @@
+package servlet.manager;
+
+import date.dao.ManagerDao;
+import date.model.Manager;
+import freemarker.TemplateProvider;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.inject.Inject;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+@WebServlet(urlPatterns = "/my-account")
+public class AccountManagerServlet extends HttpServlet {
+
+    private static final Logger LOG = LoggerFactory.getLogger(AccountManagerServlet.class);
+    private static final String TEMPLATE_NAME = "manager-my-account";
+
+    @Inject
+    private TemplateProvider templateProvider;
+    @Inject
+    private ManagerDao managerDao;
+
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html;charset=UTF-8");
+        final PrintWriter writer = resp.getWriter();
+        HttpSession session = req.getSession(true);
+
+        Map<String, Object> model = new HashMap<>();
+
+        Manager managerSession = (Manager) session.getAttribute("user");
+        model.put("user", managerSession);
+
+
+        Template template = templateProvider.getTemplate(getServletContext(), TEMPLATE_NAME);
+
+        try {
+            template.process(model, writer);
+        } catch (TemplateException e) {
+            e.printStackTrace();
+        }
+    }
+}
