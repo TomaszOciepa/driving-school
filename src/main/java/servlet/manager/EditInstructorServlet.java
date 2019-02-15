a2fb728551a41a6884cb9b5f7f36a6d85952d048
tomaszociepa <tom.ociepa@gmail.com>
2019-02-15 15:52:28 +0100

    Function added: edit instructor account

--- src/main/java/servlet/manager/EditInstructorServlet.java
+++ src/main/java/servlet/manager/EditInstructorServlet.java
@@ -2,7 +2,9 @@ package servlet.manager;
 
 import authentication.CheckExists;
 import authentication.DownloadUserToDatabase;
+import date.dao.InstructorDao;
 import date.dao.ManagerDao;
+import date.model.Instructor;
 import date.model.Manager;
 import freemarker.TemplateProvider;
 import freemarker.template.Template;
@@ -22,16 +24,15 @@ import java.io.PrintWriter;
 import java.util.HashMap;
 import java.util.Map;
 
-@WebServlet(urlPatterns = "/edit-manager")
-public class EditManagerServlet extends HttpServlet {
-
+@WebServlet(urlPatterns = "/edit-instructor")
+public class EditInstructorServlet extends HttpServlet {
     private static final Logger LOG = LoggerFactory.getLogger(EditManagerServlet.class);
-    private static final String TEMPLATE_NAME = "manager-manager-edit";
+    private static final String TEMPLATE_NAME = "manager-instructor-edit";
 
     @Inject
     private TemplateProvider templateProvider;
     @Inject
-    private ManagerDao managerDao;
+    private InstructorDao instructorDao;
     @Inject
     private CheckExists checkExists;
     @Inject
@@ -39,6 +40,7 @@ public class EditManagerServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
         resp.setContentType("text/html;charset=UTF-8");
         final PrintWriter writer = resp.getWriter();
         HttpSession session = req.getSession(true);
@@ -49,14 +51,14 @@ public class EditManagerServlet extends HttpServlet {
         model.put("user", managerSession);
 
         int id = Integer.parseInt(req.getParameter("id"));
+        Instructor editedInstructor = instructorDao.findById(id);
 
-        Manager editedManager = managerDao.findById(id);
-
-        session.setAttribute("editedManager", editedManager);
-        model.put("manager", editedManager);
+        session.setAttribute("editedInstructor", editedInstructor);
+        model.put("instructor", editedInstructor);
         Template template = templateProvider.getTemplate(getServletContext(), TEMPLATE_NAME);
 
         loadTemplate(writer, model, template);
+
     }
 
     @Override
@@ -70,44 +72,38 @@ public class EditManagerServlet extends HttpServlet {
         Manager managerSession = (Manager) session.getAttribute("user");
         model.put("user", managerSession);
 
-        Manager editedManager = (Manager) session.getAttribute("editedManager");
+        Instructor editedInstructor = (Instructor) session.getAttribute("editedInstructor");
 
         String email = req.getParameter("email");
         String name = req.getParameter("name");
         String lastName = req.getParameter("lastname");
+        String phone = req.getParameter("phone");
+        String street = req.getParameter("street");
+        String city = req.getParameter("city");
 
         Template template = templateProvider.getTemplate(getServletContext(), TEMPLATE_NAME);
 
-        if (checkExists.checkManagerExists(email)) {
+        if (checkExists.checkInstructorExists(email)) {
 
-            if (editedManager.getManagerEmail().equals(email)){
-                updateManager(editedManager, email, name, lastName);
-                LOG.info("Manager has bean update");
-                model.put("manager", editedManager);
-                model.put("SuccesUpdate", "Manager has bean update");
+            if (editedInstructor.getInstructorEmail().equals(email)){
+                updateInstructor(editedInstructor, email, name, lastName, phone, street, city);
+                LOG.info("Instructor has bean update");
+                model.put("instructor", editedInstructor);
+                model.put("SuccesUpdate", "Instructor has bean update");
                 loadTemplate(writer, model, template);
             }else {
                 LOG.info("Email is already busy");
-                model.put("manager", editedManager);
+                model.put("instructor", editedInstructor);
                 model.put("FailedUpdate", "Email is already busy. Try again.");
                 loadTemplate(writer, model, template);
             }
         } else {
-            updateManager(editedManager, email, name, lastName);
-            LOG.info("Manager has bean update");
-            model.put("manager", editedManager);
-            model.put("SuccesUpdate", "Manager has bean update");
+            updateInstructor(editedInstructor, email, name, lastName, phone, street, city);
+            LOG.info("Instructor has bean update");
+            model.put("instructor", editedInstructor);
+            model.put("SuccesUpdate", "Instructor has bean update");
             loadTemplate(writer, model, template);
         }
-
-    }
-
-    private void updateManager(Manager editedManager, String email, String name, String lastName) {
-        editedManager.setManagerEmail(email);
-        editedManager.setManagerName(name);
-        editedManager.setManagerLastname(lastName);
-
-        managerDao.update(editedManager);
     }
 
     private void loadTemplate(PrintWriter writer, Map<String, Object> model, Template template) throws IOException {
@@ -117,5 +113,16 @@ public class EditManagerServlet extends HttpServlet {
         } catch (TemplateException e) {
             LOG.warn("Failed load template manager-new-manager");
         }
+    }
+
+    private void updateInstructor(Instructor editedInstructor, String email, String name, String lastName, String phone, String street, String city) {
+        editedInstructor.setInstructorEmail(email);
+        editedInstructor.setInstructorName(name);
+        editedInstructor.setInstructorLastname(lastName);
+        editedInstructor.setInstructorPhone(phone);
+        editedInstructor.setInstructorStreet(street);
+        editedInstructor.setInstructorCity(city);
+
+        instructorDao.update(editedInstructor);
     }
 }
